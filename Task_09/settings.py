a89af303842d6f342c7583e66cb9f190c59c0e88
Rudra7124 <rudraghadai91@gmail.com>
2026-05-23 11:22:59 +0530

    Create settings.py

---
+++ Task_09/settings.py
@@ -0,0 +1,13 @@
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    )
+}
+
+BASE_URL = "https://nitter.net"
+
+REQUEST_TIMEOUT = 10
+
+RETRY_COUNT = 3
+
+MAX_TWEETS_PER_KEYWORD = 20
